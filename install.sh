3d068e71244b92a118a53892d34524b0e77244b0
Morpheusthewhite <zffromGerace@hotmail.it>
2018-07-15 10:00:03 +0200

    Now install.sh downloads conf files from nordvpn site

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 #install dependencies and add desktop Entry
-# TODO: follow instructions on nordvpn site to download config files
 
 if ! [ -z `which apt-get 2> /dev/null` ]; # Debian
-    then sudo apt-get install python python-tkinter python-requests openvpn 2> /dev/null
+    then sudo apt-get install python python-tkinter python-requests openvpn wget unzip 2> /dev/null
 fi
 if ! [ -z `which dnf 2> /dev/null` ]; # Fedora
-    then sudo dnf install python python-tkinter python-requests openvpn 2> /dev/null
+    then sudo dnf install python python-tkinter python-requests openvpn wget unzip 2> /dev/null
 fi
 if ! [ -z `which pacman 2> /dev/null` ]; # Arch Linux
-    then sudo pacman install python python-tkinter python-requests openvpn 2> /dev/null
+    then sudo pacman install python python-tkinter python-requests openvpn wget unzip 2> /dev/null
 fi
 
 current_dir=`pwd`
@@ -28,3 +27,10 @@ Terminal=false
 Categories=Internet;System;Utilities;" | sudo tee /usr/local/share/applications/nordpy.desktop > /dev/null
 
 sudo chmod 0777 /usr/local/share/applications/nordpy.desktop
+
+
+# downloading and extracting conf files from NordVPN
+cd /etc/openvpn
+sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
+sudo unzip ovpn.zip
+sudo rm zip
