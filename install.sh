5fdda2b06ca012345df8a19d0eac8501cf961d1e
morpheusthewhite <zffromGerace@hotmail.it>
2018-10-13 12:38:57 +0200

    corrected minor problems

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ Version=1.0
 Name=NordPy
 Comment=NordVPN client application for connecting to recommended servers
 Path="$current_dir"
-Exec=python3.6 "$current_dir"/nordpy.py
+Exec=python3 "$current_dir"/nordpy.py
 Icon="$current_dir"/media/nordvpn.png
 Terminal=false
 Categories=Internet;System;Utilities;" | sudo tee /usr/local/share/applications/nordpy.desktop > /dev/null
@@ -33,4 +33,4 @@ sudo chmod 0777 /usr/local/share/applications/nordpy.desktop
 cd /etc/openvpn
 sudo wget https://downloads.nordcdn.com/configs/archives/servers/ovpn.zip
 sudo unzip ovpn.zip
-sudo rm zip
+sudo rm ovpn.zip
