f9d6126d96b6b02684e80b30b4c6f92dbe796cc7
morpheusthewhite <zffromGerace@hotmail.it>
2019-04-09 15:29:32 +0200

    Replace echo with printf because of different newline treatment on different versions (see https://github.com/morpheusthewhite/NordPy/issues/17)

--- scripts/nordpy_up.sh
+++ scripts/nordpy_up.sh
@@ -3,7 +3,7 @@
 # works only if launched with sudo
 
 cd /etc/
-echo "# Appended by NordPy\nnameserver 8.8.8.8\nnameserver 8.8.4.4" > resolv.conf.tmp && sed '/nameserver/ d' resolv.conf >> resolv.conf.tmp
+printf "# Appended by NordPy\nnameserver 8.8.8.8\nnameserver 8.8.4.4\n" > resolv.conf.tmp && sed '/nameserver/ d' resolv.conf >> resolv.conf.tmp
 
 mv resolv.conf resolv.conf.backup
 mv resolv.conf.tmp resolv.conf
