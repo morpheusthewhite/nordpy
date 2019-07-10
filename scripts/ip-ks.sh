b66c86ec3b0e99fd2b60c695478ad16ae180ebeb
morpheusthewhite <zffromGerace@hotmail.it>
2019-07-10 23:08:50 +0200

    Fix connection error when VPN is started

--- scripts/ip-ks.sh
+++ scripts/ip-ks.sh
@@ -19,6 +19,8 @@ REMOTE_PORT=$2
 INTERFACE=$3
 PROTOCOL=$4
 
+echo "Launching $PROTOCOL connection with $REMOTE_IP:$REMOTE_PORT on $INTERFACE"
+
 # clear tables
 iptables --flush
 iptables --delete-chain
@@ -27,6 +29,7 @@ iptables -t nat --delete-chain
 
 # general policy
 iptables -P OUTPUT DROP
+iptables -P INPUT DROP
 
 # localhost
 iptables -A INPUT -j ACCEPT -i lo
@@ -36,5 +39,5 @@ iptables -A INPUT --src 192.168.0.0/24 -j ACCEPT -i $INTERFACE
 iptables -A OUTPUT -d 192.168.0.0/24 -j ACCEPT -o $INTERFACE
 iptables -A OUTPUT -j ACCEPT -d $REMOTE_IP -o $INTERFACE -p $PROTOCOL -m $PROTOCOL --dport $REMOTE_PORT
 iptables -A INPUT -j ACCEPT -s $REMOTE_IP -i $INTERFACE -p $PROTOCOL -m $PROTOCOL --sport $REMOTE_PORT
-iptables -A INPUT -j ACCEPT -i tun
-iptables -A OUTPUT -j ACCEPT -o tun
+iptables -A INPUT -j ACCEPT -i tun+
+iptables -A OUTPUT -j ACCEPT -o tun+
