8d134c7c0cd1371a8fbcfb95e3bab4ab2d9b4303
morpheusthewhite <zffromGerace@hotmail.it>
2020-01-21 08:02:16 +0100

    Replace Google DNS servers with NordVPN ones (close https://github.com/morpheusthewhite/nordpy/issues/29)

--- scripts/nordpy_up.sh
+++ scripts/nordpy_up.sh
@@ -3,7 +3,7 @@
 # works only if launched with sudo
 
 cd /etc/
-printf "# Appended by NordPy\nnameserver 8.8.8.8\nnameserver 8.8.4.4\n" > resolv.conf.tmp && sed '/nameserver/ d' resolv.conf >> resolv.conf.tmp
+printf "# Appended by NordPy\nnameserver 103.86.96.100\nnameserver 103.86.99.100\n" > resolv.conf.tmp && sed '/nameserver/ d' resolv.conf >> resolv.conf.tmp
 
 mv resolv.conf resolv.conf.backup
 mv resolv.conf.tmp resolv.conf
