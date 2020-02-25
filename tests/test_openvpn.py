e2e8502090b3cbedaf1c3c83740b077be15b1b80
morpheusthewhite <zffromGerace@hotmail.it>
2020-02-25 22:28:34 +0100

    Add test for openvpn connection

---
+++ tests/test_openvpn.py
@@ -0,0 +1,29 @@
+from bin.vpn_util.openvpn import start_openvpn, openvpn_stop, checkOpenVPN
+from bin.root import get_root_permissions_cli
+from .ip_show import ip_show
+
+TEST_SERVER="us3381.nordvpn.com"
+TEST_PROTOCOL = 0
+
+get_root_permissions_cli()
+
+initial_ip = ip_show()
+
+
+def test_status_before_connection():
+    assert not checkOpenVPN()
+
+
+def test_start_connection():
+    start_openvpn(TEST_SERVER, TEST_PROTOCOL, False)
+    assert initial_ip != ip_show()
+
+
+def test_status_after_connection():
+    assert checkOpenVPN()
+
+
+def test_stop_connection():
+    openvpn_stop(False)
+    assert initial_ip == ip_show()
+
