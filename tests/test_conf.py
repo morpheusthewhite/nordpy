ac10ea7135456c93950a744439c7f106eaab8d0c
morpheusthewhite <zffromGerace@hotmail.it>
2021-02-06 18:32:35 +0100

    test(conf): correct configuration file

--- tests/test_conf.py
+++ tests/test_conf.py
@@ -2,9 +2,10 @@ from bin.conf_util import *
 
 
 def test_get_path():
-    assert get_path_to_conf("ar20.nordvpn.com", 1).endswith("ovpn_tcp/ar20.nordvpn.com.tcp.ovpn")
+    assert get_path_to_conf("nl833.nordvpn.com.tcp.ovpn", 1).endswith(
+        "ovpn_tcp/nl833.nordvpn.com.tcp.ovpn")
 
 
 def test_update_conf():
-    with open(update_conf("ovpn_tcp/ar20.nordvpn.com.tcp.ovpn", {"ping-restart": 10}), 'r') as new_conf:
+    with open(update_conf("ovpn_tcp/nl833.nordvpn.com.tcp.ovpn", {"ping-restart": 10}), 'r') as new_conf:
         assert("ping-restart 10" in new_conf.read())
