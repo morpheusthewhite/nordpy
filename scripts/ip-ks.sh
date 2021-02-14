b923182bcca60ca0aa59fe092102c8e0ab3bd2f8
morpheusthewhite <zffromGerace@hotmail.it>
2021-02-14 22:04:27 +0100

    refactor(killswitch): refactor killswitch script

--- scripts/ip-ks.sh
+++ scripts/ip-ks.sh
@@ -23,46 +23,30 @@ NETWORK_ADDRESS=$5
 
 echo "Launching $PROTOCOL connection with $REMOTE_IP:$REMOTE_PORT on $INTERFACE (on network $5)"
 
+# if a iptables-legacy command is available, use it
 if [[ -z $(iptables-legacy -h) ]] ; then
-    # clear tables
-    iptables --flush
-    iptables --delete-chain
-    iptables -t nat --flush
-    iptables -t nat --delete-chain
-
-    # general policy
-    iptables -P OUTPUT DROP
-    iptables -P INPUT DROP
-
-    # localhost
-    iptables -A INPUT -j ACCEPT -i lo
-    iptables -A OUTPUT -j ACCEPT -o lo
-
-    iptables -A INPUT --src $NETWORK_ADDRESS -j ACCEPT -i $INTERFACE
-    iptables -A OUTPUT -d $NETWORK_ADDRESS -j ACCEPT -o $INTERFACE
-    iptables -A OUTPUT -j ACCEPT -d $REMOTE_IP -o $INTERFACE -p $PROTOCOL -m $PROTOCOL --dport $REMOTE_PORT
-    iptables -A INPUT -j ACCEPT -s $REMOTE_IP -i $INTERFACE -p $PROTOCOL -m $PROTOCOL --sport $REMOTE_PORT
-    iptables -A INPUT -j ACCEPT -i tun+
-    iptables -A OUTPUT -j ACCEPT -o tun+
+    IPTABLES=iptables
 else
-    # clear tables
-    iptables-legacy --flush
-    iptables-legacy --delete-chain
-    iptables-legacy -t nat --flush
-    iptables-legacy -t nat --delete-chain
-
-    # general policy
-    iptables-legacy -P OUTPUT DROP
-    iptables-legacy -P INPUT DROP
-
-    # localhost
-    iptables-legacy -A INPUT -j ACCEPT -i lo
-    iptables-legacy -A OUTPUT -j ACCEPT -o lo
-
-    iptables-legacy -A INPUT --src $NETWORK_ADDRESS -j ACCEPT -i $INTERFACE
-    iptables-legacy -A OUTPUT -d $NETWORK_ADDRESS -j ACCEPT -o $INTERFACE
-    iptables-legacy -A OUTPUT -j ACCEPT -d $REMOTE_IP -o $INTERFACE -p $PROTOCOL -m $PROTOCOL --dport $REMOTE_PORT
-    iptables-legacy -A INPUT -j ACCEPT -s $REMOTE_IP -i $INTERFACE -p $PROTOCOL -m $PROTOCOL --sport $REMOTE_PORT
-    iptables-legacy -A INPUT -j ACCEPT -i tun+
-    iptables-legacy -A OUTPUT -j ACCEPT -o tun+
+    IPTABLES=iptables-legacy
 fi
+
+# clear tables
+${IPTABLES} --flush
+${IPTABLES} --delete-chain
+${IPTABLES} -t nat --flush
+${IPTABLES} -t nat --delete-chain
+
+# general policy
+${IPTABLES} -P OUTPUT DROP
+${IPTABLES} -P INPUT DROP
+
+# localhost
+${IPTABLES} -A INPUT -j ACCEPT -i lo
+${IPTABLES} -A OUTPUT -j ACCEPT -o lo
+
+${IPTABLES} -A INPUT --src $NETWORK_ADDRESS -j ACCEPT -i $INTERFACE
+${IPTABLES} -A OUTPUT -d $NETWORK_ADDRESS -j ACCEPT -o $INTERFACE
+${IPTABLES} -A OUTPUT -j ACCEPT -d $REMOTE_IP -o $INTERFACE -p $PROTOCOL -m $PROTOCOL --dport $REMOTE_PORT
+${IPTABLES} -A INPUT -j ACCEPT -s $REMOTE_IP -i $INTERFACE -p $PROTOCOL -m $PROTOCOL --sport $REMOTE_PORT
+${IPTABLES} -A INPUT -j ACCEPT -i tun+
+${IPTABLES} -A OUTPUT -j ACCEPT -o tun+
